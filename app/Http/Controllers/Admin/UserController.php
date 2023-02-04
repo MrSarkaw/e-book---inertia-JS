2aeaee39001bb7e036b6b92554c190e63129872c
mrsarkaw <sarkaw.salar55@gmail.com>
2023-02-04 16:56:32 +0300

    user form

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,4 +12,24 @@ class UserController extends Controller
     public function index(){
         return Inertia::render('Admin/User/Index');
     }
+
+    public function create(){
+        return Inertia::render('Admin/User/Form');
+    }
+
+
+    public function store(Request $request){
+
+        $data = $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required'
+        ]);
+
+        User::create($data);
+
+        return redirect()->back();
+    }
+
+
 }
---
+++ resources/js/Pages/Admin/User/Form.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="bg-gray-100 p-2 roounded shadow">
+        <p>User Form</p>
+
+        <form @submit.prevent="store()">
+            <div class="grid grid-cols-3 gap-10 mt-10">
+                <div>
+                    <label for="">Name</label>
+                    <input v-model="form.name" type="text" class="bg-white p-2 shadow w-full">
+                </div>
+
+                <div>
+                    <label for="">Email</label>
+                    <input v-model="form.email" type="text" class="bg-white p-2 shadow w-full">
+                </div>
+
+                <div>
+                    <label for="">Password</label>
+                    <input v-model="form.password" type="password" class="bg-white p-2 shadow w-full">
+                </div>
+            </div>
+
+            <div class="mt-10">
+                <button class="p-2 rounded shadow bg-indigo-500 text-white px-5">Add</button>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+import layout from '@/layouts/admin.vue'
+import { reactive } from '@vue/reactivity'
+import { router } from '@inertiajs/core'
+export default{
+    layout:layout
+}
+
+</script>
+
+
+<script setup>
+
+let initilizeForm= {
+    name:'',
+    email:'',
+    password:'',
+}
+
+let form = reactive({...initilizeForm})
+
+const store = ()=>{
+    router.post('/admin/user', form, {
+        onSuccess: ()=>{
+            Object.assign(form, initilizeForm)
+        }
+    })
+}
+
+</script>
