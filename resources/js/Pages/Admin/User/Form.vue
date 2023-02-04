dbe11dd82f14e7e0dabe6e6af963b9216fefd933
mrsarkaw <sarkaw.salar55@gmail.com>
2023-02-04 17:03:50 +0300

    showing errors

--- resources/js/Pages/Admin/User/Form.vue
+++ resources/js/Pages/Admin/User/Form.vue
@@ -7,16 +7,19 @@
                 <div>
                     <label for="">Name</label>
                     <input v-model="form.name" type="text" class="bg-white p-2 shadow w-full">
+                    <p class="text-red-500 text-sm">{{ errors.name }}</p>
                 </div>
 
                 <div>
                     <label for="">Email</label>
                     <input v-model="form.email" type="text" class="bg-white p-2 shadow w-full">
+                    <p class="text-red-500 text-sm">{{ errors.email }}</p>
                 </div>
 
                 <div>
                     <label for="">Password</label>
                     <input v-model="form.password" type="password" class="bg-white p-2 shadow w-full">
+                    <p class="text-red-500 text-sm">{{ errors.password }}</p>
                 </div>
             </div>
 
@@ -28,7 +31,7 @@
 </template>
 <script>
 import layout from '@/layouts/admin.vue'
-import { reactive } from '@vue/reactivity'
+import { reactive, ref } from '@vue/reactivity'
 import { router } from '@inertiajs/core'
 export default{
     layout:layout
@@ -38,6 +41,8 @@ export default{
 
 
 <script setup>
+
+defineProps(['errors'])
 
 let initilizeForm= {
     name:'',
