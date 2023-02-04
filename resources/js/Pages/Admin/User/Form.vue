7664edfd05379972455d37a973999eb0c25b38a7
mrsarkaw <sarkaw.salar55@gmail.com>
2023-02-04 17:16:54 +0300

    using useform

--- resources/js/Pages/Admin/User/Form.vue
+++ resources/js/Pages/Admin/User/Form.vue
@@ -7,24 +7,24 @@
                 <div>
                     <label for="">Name</label>
                     <input v-model="form.name" type="text" class="bg-white p-2 shadow w-full">
-                    <p class="text-red-500 text-sm">{{ errors.name }}</p>
+                    <p class="text-red-500 text-sm">{{ form.errors.name }}</p>
                 </div>
 
                 <div>
                     <label for="">Email</label>
                     <input v-model="form.email" type="text" class="bg-white p-2 shadow w-full">
-                    <p class="text-red-500 text-sm">{{ errors.email }}</p>
+                    <p class="text-red-500 text-sm">{{ form.errors.email }}</p>
                 </div>
 
                 <div>
                     <label for="">Password</label>
                     <input v-model="form.password" type="password" class="bg-white p-2 shadow w-full">
-                    <p class="text-red-500 text-sm">{{ errors.password }}</p>
+                    <p class="text-red-500 text-sm">{{ form.errors.password }}</p>
                 </div>
             </div>
 
             <div class="mt-10">
-                <button class="p-2 rounded shadow bg-indigo-500 text-white px-5">Add</button>
+                <button :disabled="form.processing" class="p-2 rounded shadow bg-indigo-500 text-white px-5">Add</button>
             </div>
         </form>
     </div>
@@ -33,6 +33,7 @@
 import layout from '@/layouts/admin.vue'
 import { reactive, ref } from '@vue/reactivity'
 import { router } from '@inertiajs/core'
+import { useForm } from '@inertiajs/vue3'
 export default{
     layout:layout
 }
@@ -42,20 +43,16 @@ export default{
 
 <script setup>
 
-defineProps(['errors'])
-
-let initilizeForm= {
+let form = useForm({
     name:'',
     email:'',
     password:'',
-}
-
-let form = reactive({...initilizeForm})
+})
 
 const store = ()=>{
-    router.post('/admin/user', form, {
+    form.post('/admin/user',{
         onSuccess: ()=>{
-            Object.assign(form, initilizeForm)
+            form.reset();
         }
     })
 }
